9eab9179c0168a0f14f76529fd954c14cde5c635
SlickJohnson <liwa.johnson@gmail.com>
2019-02-26 14:13:45 -0800

    Add and configure redux and .gitignore

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
